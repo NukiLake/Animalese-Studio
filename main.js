475c4c6dcb0f9b25db00638221e74be2ea563a8a
Purtan <markpurtan@gmail.com>
2026-05-25 02:48:17 -0700

    Add initial Electron app scaffold
    Add the Animalese Studio project skeleton: Electron main.js entry, index.html UI, UI assets, .gitignore, IDE/project files (.idea and .sln) and basic frontend scripts. Also include installed dependencies and node_modules with a package-lock, providing a runnable initial setup for development.

---
+++ main.js
@@ -0,0 +1,35 @@
+﻿const path = require('path');
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+    const iconName = process.platform === 'win32' ? 'icon.ico' : 'icon.png';
+    const iconPath = path.join(__dirname, 'assets', iconName);
+    const win = new BrowserWindow({
+        width: 1000,
+        height: 700,
+        webPreferences: {
+            nodeIntegration: true
+        },
+        icon: iconPath,
+        autoHideMenuBar: true
+    });
+
+
+    win.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
---
+++ renderer.js
@@ -0,0 +1,44 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+    Build();
+});
+
+function Build() {
+    console.log("TEST2");
+
+    //const dropzone = document.getElementById('dropzone');
+    const input = document.getElementById('input');
+    console.log("HI");
+    /*// Prevent default browser behavior for file drag/drop across the window
+    ['dragover', 'drop'].forEach(ev => {
+        window.addEventListener(ev, e => e.preventDefault());
+    });
+
+    // Visual feedback
+    ['dragenter','dragover'].forEach(evt =>
+        dropzone.addEventListener(evt, e => {
+            e.preventDefault();
+            dropzone.classList.add('dragover');
+        })
+    );
+    ['dragleave','drop'].forEach(evt =>
+        dropzone.addEventListener(evt, e => {
+            e.preventDefault();
+            dropzone.classList.remove('dragover');
+        })
+    );*/
+
+    // Add "Selected" class to each file-listing when one is clicked
+    var fileListings = document.getElementsByClassName('file-listing');
+    for (let i = 0; i < fileListings.length; i++) {
+        fileListings[i].addEventListener('click', function() {
+
+            for (let j = 0; j < fileListings.length; j++) {
+                fileListings[j].classList.remove('selected');
+            }
+            this.classList.add('selected');
+
+            // log
+            console.log('Selected file:', this.textContent);
+        });
+    }
+}
